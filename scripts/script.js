0d9198b955dd5376c81cff0de6cc93522c3d316b
Michael Montgomery <thehealthyprogrammer@outlook.com>
2022-02-28 20:01:52 -0500

    added functionality for clear form button.

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,6 @@
-function OldUser(name, email, accessLevel) {
-    this.dateCreated = new Date();
-    this.userName = name;
-    this.email = email;
-    this.access = accessLevel;
-}
-
-new OldUser('Donald', 'me@yahoo.com');
-
-class User {
-    constructor(name, email) {
-        this.dateCreated = new Date();
-        this.userName = name;
-        this.email = email;
-        this.access = 'standard';
-    }
-    sendEmail() {
-        console.log(`An email was sent to ${this.userName}`)
-    }
-}
-
-class Admin extends User {
-   super(adminID) {
-       this._id = adminID;
-   }
-    deleteData() {
-        console.log(`${this.userName} has deleted data!`)
-    }
-}
-
-let john = new User('john', 'some@email.com')
-let jacob = new Admin('jacob', 'some@yahoo.com')
-
+document.querySelector('.clear-form-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelectorAll('#contact input, #contact textarea').forEach((nPut) => {
+        nPut.value = '';
+    })
+})
